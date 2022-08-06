4db6676f959472e864573a99165059fd1dadaad7
koske0125 <kousuke.0125.dm@gmail.com>
2022-08-06 13:48:31 +0900

    [Finish]JavaScript15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,3 +1,9 @@
+function delay(n) {
+  return new Promise(function(resolve){
+    setTimeout(resolve,n*1000);
+  });
+}
+
 async function getData() {
   // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
   // ３秒後にフルネームのリストを表示されるようにしましょう。
@@ -6,11 +12,13 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await userList.map(test)
+  await delay(3);
   return await result
 }
 
-function test() {
-  return
+function test(data) {
+  let full_name = data.family_name + " " + data.first_name;
+  data.full_name = full_name;
+  return data;
 }
-
